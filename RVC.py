4b450aa1c1edf41a56e30af3551128fd17c1451d
Skywalker290 <dev.ayusshh@gmail.com>
2024-08-30 00:30:57 +0530

    latest

--- RVC.py
+++ RVC.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
 from colorama import Fore
 from functions import *
-import os
 from flask import request
 
-DIR =   "/home/ubuntu/RealVC/"
+# Load environment variables from the .env file
+load_dotenv()
+
+# Get the home directory dynamically
+HOME_DIR = os.path.expanduser("~")
+DIR = f"{HOME_DIR}/RealVC/"
+
+# Retrieve the public IP from the .env file
+PUBLIC_IP = os.getenv('PUBLIC_IP')
 
 def RVC(request):
     data = request.get_json()
@@ -11,12 +20,12 @@ def RVC(request):
     voice_name = data.get('voice_name')
     
     file_path = check_file_exist(file_path)
-    output_path = RVC_gen(file_path,voice_name)
+    output_path = RVC_gen(file_path, voice_name)
 
-    return f"13.200.241.87/get-file/{output_path.split('/')[-1]}"
+    return f"{PUBLIC_IP}/get-file/{output_path.split('/')[-1]}"
 
 
-def RVC_gen(input_path,model_name):
+def RVC_gen(input_path, model_name):
     if not model_name:
         model_name = "Amitabh-Voice"
     if not input_path:
@@ -24,37 +33,34 @@ def RVC_gen(input_path,model_name):
     LAST_DIR = os.getcwd()
     os.chdir(f"{DIR}RVC")
 
-    model_path = f"{DIR}RVC/assets/weights/{model_name}.pth"#@param {type:"string"}
-    index_path = f"{DIR}RVC/logs/{model_name}/"#@param {type:"string"}
-    index_path = Prefix_find(index_path,"added")
+    model_path = f"{DIR}RVC/assets/weights/{model_name}.pth"
+    index_path = f"{DIR}RVC/logs/{model_name}/"
+    index_path = Prefix_find(index_path, "added")
 
     print(Fore.GREEN + f"{index_path} was found") if os.path.exists(index_path) else print(Fore.RED + f"{index_path} was not found")
-    pitch = 0 # @param {type:"slider", min:-12, max:12, step:1}
-
-    # input_path = f"{DIR}RVC/audios/astronauts.mp3"#@param {type:"string"}
+    pitch = 0  # @param {type:"slider", min:-12, max:12, step:1}
 
     if not os.path.exists(input_path):
         raise ValueError(f"{input_path} was not found in your RVC folder.")
 
-    os.environ['index_root']  = os.path.dirname(index_path)
+    os.environ['index_root'] = os.path.dirname(index_path)
     index_path = os.path.basename(index_path)
-    f0_method = "rmvpe" # @param ["rmvpe", "pm", "harvest"]
-    # save_as = f"{DIR}RVC/audios/cli_output.wav"#@param {type:"string"}
-    save_as = "/home/ubuntu/Server/Output_mp3/"+gen_name()+".mp3"
+    f0_method = "rmvpe"  # @param ["rmvpe", "pm", "harvest"]
+    save_as = f"{HOME_DIR}/Server/Output_mp3/" + gen_name() + ".mp3"
     model_name = os.path.basename(model_path)
     os.environ['weight_root'] = os.path.dirname(model_path)
-    index_rate = 0.5 # @param {type:"slider", min:0, max:1, step:0.01}
-    volume_normalization = 0 #param {type:"slider", min:0, max:1, step:0.01}
-    consonant_protection = 0.5 #param {type:"slider", min:0, max:1, step:0.01}
+    index_rate = 0.5  # @param {type:"slider", min:0, max:1, step:0.01}
+    volume_normalization = 0  # param {type:"slider", min:0, max:1, step:0.01}
+    consonant_protection = 0.5  # param {type:"slider", min:0, max:1, step:0.01}
 
     import subprocess
     command = f"rm -f {save_as}"
     result = subprocess.run(command, shell=True, check=True)
 
     command = f"python {DIR}RVC/tools/cmd/infer_cli.py --f0up_key {pitch} --input_path {input_path} --index_path {index_path} --f0method {f0_method} --opt_path {save_as} --model_name {model_name} --index_rate {index_rate} --device 'cuda:0' --is_half True --filter_radius 3 --resample_sr 0 --rms_mix_rate {volume_normalization} --protect {consonant_protection}"
-    result = subprocess.run(command,shell=True, check=True)
+    result = subprocess.run(command, shell=True, check=True)
 
-    print("Output File Generated: ",save_as)
+    print("Output File Generated: ", save_as)
     os.chdir(LAST_DIR)
     
     return save_as
@@ -67,13 +73,14 @@ def Prefix_find(root_path, prefix):
                 return os.path.join(dirpath, filename)
     return None
 
+
 def check_file_exist(download_path):
-    downloads = "/home/ubuntu/Server/Output_mp3/"
+    downloads = f"{HOME_DIR}/Server/Output_mp3/"
     file_name = download_path.split('/')[-1]
 
-    if not os.path.exists(downloads+file_name):
-        command = "wget -P /home/ubuntu/Server/Output_mp3/ {download_path}"
+    if not os.path.exists(downloads + file_name):
+        command = f"wget -P {downloads} {download_path}"
         result = subprocess.run(command, shell=True, check=True)
-        print(f"downloaed file {file_name}")
+        print(f"Downloaded file {file_name}")
 
-    return downloads+file_name
+    return downloads + file_name
