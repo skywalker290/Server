a31b74a8eb65a1b540807a56f461ec015e3e89a7
Skywalker290 <dev.ayusshh@gmail.com>
2024-09-23 00:25:37 +0530

    websocket

--- app.py
+++ app.py
@@ -1,129 +1,118 @@
-from flask import Flask, request, send_from_directory, render_template
-import os
+from flask import Flask, request, jsonify, send_file, render_template
 from cloner import *
-from flask import Flask, send_file
 from functions import *
-from flask_cors import CORS, cross_origin
 from RVC import *
+from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__, static_url_path='/Output_mp3')
 CORS(app)
 
+# Initialize SocketIO
+socketio = SocketIO(app, cors_allowed_origins="*")
+
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # 100 MB
 
-@app.route("/")
-def hello():
-    return gen_json("Helloo There!")
+# Event for WebSocket connection
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+    emit('response', {'message': 'You are connected!'})
 
-@app.route("/GenerateVoice/", methods=['POST'])
-def clone():
-    check = check_credentials(request)
-    if(check != True):
-        return check
-    return gen_json(Cloner(request=request))
+# Event for WebSocket disconnection
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
-@app.route('/get-file/<filename>', methods=['GET'])
-def get_file(filename):
-    filename='Output_mp3/'+filename 
-    return send_file(filename, as_attachment=True)
+# WebSocket event to send a greeting message (original "/" route)
+@socketio.on('hello')
+def hello_via_socket(data):
+    emit('response', {'message': 'Helloo There!'})
 
-@app.route('/list-speakers/',methods=['GET'])
-def list_speakers():
-    check = check_credentials(request)
-    if(check != True):
-        return check
-    return list_files()
+# WebSocket event for GenerateVoice (original "/GenerateVoice/" route)
+@socketio.on('GenerateVoice')
+def clone_via_socket(data):
+    check = check_credentials(data)
+    if check != True:
+        emit('response', {'error': check})
+    else:
+        result = Cloner(request=data)
+        emit('response', {'result': result})
 
-@app.route('/refresh-speakers/',methods=['GET'])
-def refresh_speakers():
-    check = check_credentials(request)
-    if(check != True):
-        return check
-    git_pull("Input_wavs")
-    return list_files()
+# WebSocket event to get file (original "/get-file/<filename>" route)
+@socketio.on('get_file')
+def get_file_via_socket(data):
+    filename = 'Output_mp3/' + data.get('filename', '')
+    try:
+        emit('file', {'file_url': filename})
+    except Exception as e:
+        emit('response', {'error': str(e)})
 
-@app.route('/Cloner/',methods=['POST'])
-def rvc():
-    check = check_credentials(request)
-    if(check != True):
-        return check
-    return gen_json(RVC(request))
+# WebSocket event to list speakers (original "/list-speakers/" route)
+@socketio.on('list_speakers')
+def list_speakers_via_socket(data):
+    check = check_credentials(data)
+    if check != True:
+        emit('response', {'error': check})
+    else:
+        speakers_list = list_files()
+        emit('response', {'speakers': speakers_list})
 
-@app.route('/languages/',methods=['GET'])
-def show_languages():
+# WebSocket event to refresh speakers (original "/refresh-speakers/" route)
+@socketio.on('refresh_speakers')
+def refresh_speakers_via_socket(data):
+    check = check_credentials(data)
+    if check != True:
+        emit('response', {'error': check})
+    else:
+        git_pull("Input_wavs")
+        speakers_list = list_files()
+        emit('response', {'speakers': speakers_list})
+
+# WebSocket event for Cloner (original "/Cloner/" route)
+@socketio.on('Cloner')
+def rvc_via_socket(data):
+    check = check_credentials(data)
+    if check != True:
+        emit('response', {'error': check})
+    else:
+        result = RVC(request=data)
+        emit('response', {'result': result})
+
+# WebSocket event to list languages (original "/languages/" route)
+@socketio.on('languages')
+def show_languages_via_socket():
     text = {
-        "gu": {
-            "et": "Gujarati",
-            "lt": "ગુજરાતી"
-        },
-        "hi": {
-            "et": "Hindi",
-            "lt": "हिंदी"
-        },
-        "kn": {
-            "et": "Kannada",
-            "lt": "ಕನ್ನಡ"
-        },
-        "ml": {
-            "et": "Malayalam",
-            "lt": "മലയാളം"
-        },
-        "mni": {
-            "et": "Manipuri",
-            "lt": "মণিপুরী"
-        },
-        "mr": {
-            "et": "Marathi",
-            "lt": "मराठी"
-        },
-        "or": {
-            "et": "Oriya",
-            "lt": "ଓଡ଼ିଆ"
-        },
-        "pa": {
-            "et": "Punjabi",
-            "lt": "ਪੰਜਾਬੀ"
-        },
-        "raj": {
-            "et": "Rajasthani",
-            "lt": "राजस्थानी"
-        },
-        "ta": {
-            "et": "Tamil",
-            "lt": "தமிழ்"
-        },
-        "te": {
-            "et": "Telugu",
-            "lt": "తెలుగు"
-        },
-        "as": {
-            "et": "Assamese",
-            "lt": "অসমীয়া"
-        },
-        "bn": {
-            "et": "Bengali",
-            "lt": "বাংলা"
-        },
-        "brx": {
-            "et": "Bodo",
-            "lt": "बर'"
-        }
+        "gu": {"et": "Gujarati", "lt": "ગુજરાતી"},
+        "hi": {"et": "Hindi", "lt": "हिंदी"},
+        "kn": {"et": "Kannada", "lt": "ಕನ್ನಡ"},
+        "ml": {"et": "Malayalam", "lt": "മലയാളം"},
+        "mni": {"et": "Manipuri", "lt": "মণিপুরী"},
+        "mr": {"et": "Marathi", "lt": "मराठी"},
+        "or": {"et": "Oriya", "lt": "ଓଡ଼ିଆ"},
+        "pa": {"et": "Punjabi", "lt": "ਪੰਜਾਬੀ"},
+        "raj": {"et": "Rajasthani", "lt": "राजस्थानी"},
+        "ta": {"et": "Tamil", "lt": "தமிழ்"},
+        "te": {"et": "Telugu", "lt": "తెలుగు"},
+        "as": {"et": "Assamese", "lt": "অসমীয়া"},
+        "bn": {"et": "Bengali", "lt": "বাংলা"},
+        "brx": {"et": "Bodo", "lt": "बर'"}
     }
+    emit('response', {'languages': text})
 
-    return jsonify(text), 200
-
-@app.route('/upload')
-def upload_filee():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      file_path = os.path.join("Output_mp3", f.filename)
-      f.save(file_path)
-      return 'file uploaded successfully'
+# WebSocket event for file upload (original "/uploader" route)
+@socketio.on('upload_file')
+def upload_file_via_socket(data):
+    try:
+        f = data['file']
+        filename = f.filename
+        file_path = os.path.join("Output_mp3", filename)
+        f.save(file_path)
+        emit('response', {'message': 'File uploaded successfully', 'file_url': file_path})
+    except Exception as e:
+        emit('response', {'error': str(e)})
 
 if __name__ == "__main__":
-    app.run()
-
+    # Use SocketIO to run the app with WebSocket support
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
